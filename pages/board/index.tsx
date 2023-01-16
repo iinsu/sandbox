1a76138c15814ee2865f4fd5bc96799b6fe608fd
Insu Hong <118152974+iinsu@users.noreply.github.com>
2023-01-16 17:04:54 +0900

    Delete useState

--- pages/board/index.tsx
+++ pages/board/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import TodoList from "../../components/TodoList";
 import { TodoProvider } from "../../components/TodoContext";
 import TodoCreate from "../../components/TodoCreate";
@@ -6,8 +5,6 @@ import TodoTemplate from "../../components/TodoTemplate";
 import TodoHead from "../../components/TodoHead";
 
 const Board = () => {
-  const [data, setData] = useState([]);
-
   return (
     <TodoTemplate>
       <TodoProvider>
